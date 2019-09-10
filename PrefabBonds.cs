23ba0962f92f6ba5fac8f1d31b5a32fc91a3fd49
DavidConway <35799037+DavidConway@users.noreply.github.com>
2019-09-10 16:26:49 +0100

    Create PrefabBonds.cs

---
+++ PrefabBonds.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabBonds : MonoBehaviour
+{
+    static private Bounds result;
+
+    static public Bounds redererBonds(GameObject i)
+    {
+        result = new Bounds(i.transform.position, new Vector3(0, 0,0));
+        growRederBonds(i);
+        return result;
+    }
+
+    static void growRederBonds(GameObject i)
+    {
+        List<GameObject> children = new List<GameObject>();
+
+        if (i.GetComponent<Renderer>() != null)
+        {
+            result.Encapsulate(i.GetComponent<Renderer>().bounds);
+        }
+
+        foreach (Transform j in i.transform)
+        {
+            children.Add(j.gameObject);
+        }
+
+        foreach (GameObject k in children)
+        {
+            growRederBonds(k);
+        }
+    }
+}
